4a2162974eeb87de3538d145b29d0ccde1d8b030
NATHAN MCAULEY <L8NAT92@GMAIL.COM>
2021-06-16 23:27:32 +0100

    functions_lab

---
+++ start_point/src/python_functions_practice.py
@@ -0,0 +1,49 @@
+def return_10():
+    return 10
+
+def add (num1,num2):
+    return num1 + num2
+
+def subtract(num1 , num2):
+    return num1 - num2
+
+def multiply (num1, num2):
+    return num1 * num2
+
+def divide(num1, num2):
+    return num1 / num2
+
+def length_of_string (string_length):
+    return len (string_length)
+
+def join_string(string_1, string_2):
+    return string_1 + string_2
+
+def add_string_as_number (string_1, string_2):
+    return int(string_1) + int(string_2)
+
+def number_to_full_month_name(num):
+    if num == 1:
+        return "January"
+    elif num == 3:
+        return "March"
+    elif num == 9:
+        return "September"
+def number_to_short_month_name(num):
+    if num == 1:
+        return "Jan"
+    elif num == 4:
+        return "Apr"
+    elif num == 10:
+        return "Oct"
+
+def volume_of_cube(num):
+    return num * num * num
+
+
+def reverse_string (string_rev):
+    return reversed(string_rev)
+
+
+
+
